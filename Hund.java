efe46021d4a5045fc1e63ec17421dd30e5157915
salomon <salomonxt@protonmail.ch>
2025-09-01 17:16:43 +0200

    Add files via upload

---
+++ Hund.java
@@ -0,0 +1,5 @@
+public class Hund extends Tier {
+    void bellen() {
+        System.out.println("Der Hund bellt.");
+    }
+}
---
+++ Tier.java
@@ -0,0 +1,5 @@
+public class Tier {
+    void essen() {
+        System.out.println("Das Tier isst.");
+    }
+}
